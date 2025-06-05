daed98fa7eb3d1b68d6db43028987465edae34af
FaridStitou <faridstitou1890@gmail.com>
2025-06-05 19:02:23 +0200

    chabges in concat

--- 4-concat.js
+++ 4-concat.js
@@ -3,7 +3,7 @@ const args = process.argv.slice(2);
 if (args[0] === undefined) {
   console.log("undefined is undefined");
 } else if(args[0] !==undefined && args[1] === undefined){
-  console.log(args[0] + " is undefiend")
+  console.log(args[0] + " is undefined")
 }else{
   console.log(args[0] + " is " + args[1]);
 }
