5908a376a38e7632bf0a5ce94a5110a7c2ec1060
FaridStitou <faridstitou1890@gmail.com>
2025-06-05 18:59:52 +0200

    file 4 concat

---
+++ 4-concat.js
@@ -0,0 +1,9 @@
+const args = process.argv.slice(2);
+
+if (args[0] === undefined) {
+  console.log("undefined is undefined");
+} else if(args[0] !==undefined && args[1] === undefined){
+  console.log(args[0] + " is undefiend")
+}else{
+  console.log(args[0] + " is " + args[1]);
+}
