0726a12530278e0c6996a6cbf5f110188ded44cb
FaridStitou <faridstitou1890@gmail.com>
2025-06-05 22:18:01 +0200

    correct error

--- 10-factorial.js
+++ 10-factorial.js
@@ -1,14 +1,11 @@
 const args = process.argv.slice(2);
 let a = Number(args[0]);
-const fact = n =>{ 
-  if (n < 0) return undefined;
-  else if (n === 0 || n === 1) return 1; 
-  return n * fact(n - 1); // Recursive case
+const fact = n =>{
+  if(Number.isInteger(n)) {
+  if (n === 0 || n === 1) return 1;
+  return n * fact(n - 1);
+  }else{
+  return NaN;
 }
-if (Number.isInteger(a)) {  
-  console.log(fact(a));
-}else{
-  console.log(NaN);
 }
-
-
+console.log(fact(a));
